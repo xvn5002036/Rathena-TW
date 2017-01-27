0e6dc75ed3ad11dd3d7983a56914707db54534a2
Rathena TW <xvn5002036@gmail.com>
2017-01-28 03:24:58 +0800

    1.6.3版本

---
+++ tools/serv_bat/mapserv-pre.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+title rAthenaCN 地圖服務器 - 復興前
+CALL tools\serv_bat\serv.bat map-server-pre.exe 地圖服務器
---
+++ tools/serv_bat/mapserv.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+title rAthenaCN 地圖服務器 - 復興後
+CALL tools\serv_bat\serv.bat map-server.exe 地圖服務器
